a2d181d41d7858e3f6bd6c846b8d0fa2db722a4e
darthcurtiscass <114704942+darthcurtiscass@users.noreply.github.com>
2022-11-01 18:22:15 -0600

    added JS functionality

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,24 @@
 // Assignment code here
 
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+function generatePassword() {
+  var passLength = prompt('how many characters?');
+  var genPassword = ''
+  console.log(passLength);
+  return genPassword
+}
+
 function writePassword() {
-  var password = generatePassword();
+  var password = generatePassword(); // =password
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
+
+console.log(password);
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
