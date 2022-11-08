0fef6b151f57c780dbad18085fe23480076322b0
darthcurtiscass <114704942+darthcurtiscass@users.noreply.github.com>
2022-11-08 06:27:53 -0700

    added prompts and minor functionality.

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,40 @@
 // Assignment code here
-
+var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function generatePassword() {
-  var passLength = prompt('how many characters?');
-  var genPassword = ''
-  console.log(passLength);
-  return genPassword
+
+const random = Math.floor(Math.random() * 3);
+
+const getRandomPass = () => {
+  if (random === 0) {
+    return 'a';
+  } else if (random === 1) {
+    return 'b';
+  } else if (random === 2) {
+    return 'c'
+  }
 }
+
+// variable
+function generatePassword() {
+  var passwordLength = prompt('how many characters?');
+ 
+    if (passwordLength >= 8 && passwordLength <= 120) {
+    prompt('Would you like your password to contain letters and numbers? yes or no?');
+      if ('yes') {
+        console.log("I'm lost");
+      } else if ('no') {
+        prompt("I guess you don't like security");
+      }
+    }
+};
 
 function writePassword() {
   var password = generatePassword(); // =password
   var passwordText = document.querySelector("#password");
-
+  
   passwordText.value = password;
 }
 
