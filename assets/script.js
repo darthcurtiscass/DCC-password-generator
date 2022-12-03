cf8af57dfd768ee02a905889eec2de330710cfb8
darthcurtiscass <114704942+darthcurtiscass@users.noreply.github.com>
2022-12-02 18:33:51 -0700

    fixed random password generator. Added better for loop and functions

--- assets/script.js
+++ assets/script.js
@@ -1,61 +1,79 @@
 // Assignment code here
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate")
 
-// Write password to the #password input
+var genPassword = ''
 
-//const random = Math.floor(Math.random() * 3);
-
-var charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$!&|';
-var genPassword = ''   
-var securePassword = ''
-var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-var lowercase = 'abcdefghijklmnopqrstuvwxyz'
-var numbers = '0123456789'
-var spec = '@#$!&|%^*(){}/?`~<>'
-// variable
-function generatePassword() {
-  var passwordLength = prompt('how many characters? (Must be between 8 & 120 characters)');
- 
-    if (passwordLength >= 8 && passwordLength <= 128) {
-    var promptAnswer = prompt('Would you like your password to contain letters and numbers? yes or no?');
-      if (promptAnswer === 'yes') {
-        for (let i = 0; i <= passwordLength; i++) {
-          var pass = Math.floor(Math.random()
-            * charset.length);
-          genPassword += charset.charAt(pass);
-      }
-      return genPassword;
-          } else if (promptAnswer === 'no') {
-            alert("I guess you don't like secure passwords. Password must contain Uppercase, lowercase, and special characters.");
-          }
-    } else {
-      alert("I guess you don't like secure passwords.");
-    }
-};
-
+// calling generatePassword and populating the password into the text field with id of #password
 function writePassword() {
   var password = generatePassword(); // =password
   var passwordText = document.querySelector("#password");
-  
-  passwordText.value = password;
+
+  passwordText.value = genPassword;
+
 }
+// checking the length answered to the prompt and returning the length
+function validateLength(){
+  var tempLength = prompt("whats the length?")
+  if(tempLength > 7 && tempLength < 129) {
+    return tempLength
+  }
+  var tryAgain = confirm("Do you want to try length again?")
+  if(!tryAgain){
+    return false
+  }
+  return validateLength()
+}
+// Checking the length, cheking the characters that the user would like in their password, using a for loop to generate the password
+function generatePassword(){
+  var length = validateLength()
+  // console.log("Creating password length variable: ", length)
+  if(!length){
+    alert("Invalid password length. length must be between 8 and 128 characters")
+    return
+  }
+  var Characters = possibleCharacters()
+  if(!Characters) {
+    alert("You didn't select any characters")
+  } else {
+    for (let i = 0; i < length; i++) {
+      var pass = Math.floor(Math.random() * Characters.length);
+          genPassword += Characters.charAt(pass)
+    }}
+  }
 
-console.log(password);
-
+function possibleCharacters(){
+  var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  var lowercase = 'abcdefghijklmnopqrstuvwxyz'
+  var numbers = '0123456789'
+  var spec = '@#$!&|%^*(){}/?`~<>'
+  var base = ""  
+  var wantsUpper = confirm("Do you want uppercase?")
+  if(wantsUpper){
+    base += uppercase
+  }
+  var wantsLower = confirm("Lower?")
+  if(wantsLower) {
+    base += lowercase
+  }
+  var wantsNumbers = confirm("Numbers?")
+  if(wantsNumbers) {
+    base += numbers
+  }
+  var wantsSpec = confirm("Special characters?")
+  if(wantsSpec) {
+   return base += spec
+  } 
+  console.log(base)
+  //add: if statements for both numbers and spec
+  
+  if(base.length === 0){
+    var tryAgain = confirm("Do you want to try base again?")
+    if(!tryAgain){
+      return
+    }
+    return possibleCharacters()  
+  }
+}
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-
-// return genPassword;
-//       } else if (promptAnswer === 'no') {
-//         var upQ = prompt("Include Uppercase?");
-//         if (upQ === '') {
-//           var lowQ = prompt('Include lowercase?');
-//           if (lowQ === ''){
-//             var spQ = prompt('Include special characters?');
-//             if (spQ === ''){
-//               for (let i = 0; i <=passwordLength; i++) {
-//                 var secPass = Math.floor(Math.random() * charset.length);
-//                 securePassword === charset.charAt(secPass);
-//               }
+generateBtn.addEventListener("click", writePassword)
